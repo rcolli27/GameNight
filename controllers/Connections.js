eecb6c7264a6323b3798def3df6941ca71e06b94
Ryan Collins <ryan.collins727@gmail.com>
2020-02-29 23:22:27 -0500

    Small change to Connections.js format to prevent error from sending when connections.ejs was loaded after an ID was chosen

--- controllers/Connections.js
+++ controllers/Connections.js
@@ -3,16 +3,16 @@ var router = express.Router();
 
 var Connection = require('../utilities/connectionDB');
 
+let connections = Connection.getConnections(); //Gets list of connections
+let categories = [];
+for (conn of connections) {                     //Gets list of categories by iterating through each connection, reading the category type, then adding it to categories
+    if (categories.indexOf(conn.type) == -1) {  //if it doesn't already exist
+        categories.push(conn.type);
+    }
+}
+
 //no parameters given
 router.get('/', function (req, res) {
-    let connections = Connection.getConnections(); //Gets list of connections
-    let categories = [];
-    for (conn of connections) {                     //Gets list of categories by iterating through each connection, reading the category type, then adding it to categories
-        if (categories.indexOf(conn.type) == -1) {  //if it doesn't already exist
-            categories.push(conn.type);
-        }
-    }
-    console.log(categories);
     if (Object.keys(req.query).length == 0 || Object.keys(req.query).length > 1) { //No parameter or invalid number of parameters
         res.render('connections', { connections: connections, categories: categories });
     } else {
@@ -21,10 +21,9 @@ router.get('/', function (req, res) {
 });
 
 router.get('/:id', function (req, res) {
-    let connections = Connection.getConnections();
     let conn = Connection.getConnection(req.params.id);
     if (conn == -1) { //can't find the id in the available list
-        res.render('connections', { connections: connections });
+        res.render('connections', { connections: connections, categories: categories });
     } else {
         res.render('connection', { conn: conn });
     }
