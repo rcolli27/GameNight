7a1eddd162f28247f63617c64a70c16a7b351cc0
Ryan Collins <ryan.collins727@gmail.com>
2020-04-24 16:53:19 -0400

    Spacing and Formatting

--- controllers/Connections.js
+++ controllers/Connections.js
@@ -10,21 +10,18 @@ router.get('/', async function (req, res) {
 
     let connections = await Connection.getConnections(); //Gets list of connections
     
-    /*let categories = [];
+    let categories = [];
     for (conn of connections) {                     //Gets list of categories by iterating through each connection, reading the category type, then adding it to categories
         if (categories.indexOf(conn.type) == -1) {  //if it doesn't already exist
             categories.push(conn.type);
         }
     }
 
-
-
-
     if (Object.keys(req.query).length == 0 || Object.keys(req.query).length > 1) { //No parameter or invalid number of parameters
         res.render('connections', { connections: connections, categories: categories, user: req.session.user });
     } else {
          
-    }*/
+    }
 });
 
 router.get('/:id', function (req, res) {
