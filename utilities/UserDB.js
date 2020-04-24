aa56246cfe7b66ad89c8cbc43d2a520d5f489532
Ryan Collins <ryan.collins727@gmail.com>
2020-04-24 12:47:37 -0400

    Modified UserDB to use the mongo collection and get the User instead of the UserProfile

--- utilities/UserDB.js
+++ utilities/UserDB.js
@@ -1,7 +1,5 @@
-var UserProfile = require('../models/UserProfile');
-var Connection = require('../models/connection');
 var User = require('../models/User');
-var UserConnection = require('../models/UserConnection');
+
 /*
 let det1 = "Scrabble with friends!";
 let det2 = "Monopoly with friends!";
@@ -11,8 +9,30 @@ let con2 = new Connection(2, 'Board', 'Monopoly', det2, "2020-03-08T13:00", "Woo
 
 let user = new UserProfile(new User(1, "Ryan", "Collins", "rcolli27@uncc.edu"), [new UserConnection(con1, 'yes'), new UserConnection(con2, 'yes')]);
 */
-function getUserProfile() {
-    return user;
+
+var mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+    _id: Number,
+    first: String,
+    last: String,
+    email: String
+});
+
+let userModel = mongoose.model("Users", userSchema);
+
+async function getUser(username) {       //Finds the User given the username input
+    return new Promise((resolve, reject) => {
+        userModel.find({ email: username })     // Search Users collection for the email attributed with the user
+            .then((data) => {
+                let userObj = new User(data._id, data.first, data.last, data.email);
+
+                resolve(userObj);
+            })
+            .catch((err) => {
+                return reject(err);
+            });
+    });
 }
 
-module.exports = getUserProfile;
+module.exports = getUser;
