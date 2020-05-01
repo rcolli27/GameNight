38048782a86dfa7d5de130d65fe7f0881fc1263d
Ryan Collins <ryan.collins727@gmail.com>
2020-05-01 00:11:01 -0400

    Sends the user to their savedConnections page if they are logged in.
    login page now includes errors if there are any errors in the form.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,6 +7,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var userProfile = require("../models/UserProfile");
 
+const { check, validationResult } = require('express-validator');
+
 var connectionDB = require("../utilities/connectionDB");
 var UserDB = require('../utilities/UserDB');
 var UserProfileDB = require('../utilities/UserProfileDB');
@@ -18,21 +20,38 @@ router.get('/logout', function (req, res) {         //log out the user
 });
 
 router.get('/', function (req, res) {               //load log in page
-    res.render('login', {user: req.session.user});
+    if (req.session.user) return res.redirect('/savedConnections'); //Send the user to their profile if they are logged in
+
+    res.render('login', {user: req.session.user, errors: null});
 });
 
-router.post('/', urlencodedParser, async function (req, res) {              //log in the user
+router.post('/', urlencodedParser, [check('username').isEmail(), check('password').isLength({ min: 5 })],
+    async function (req, res) {              //log in the user
 
-    let user;
+        let errs = [];
 
-    if (!req.body.username) user = await UserDB("rcolli27@uncc.edu");       //use rcolli27@uncc.edu as default or the passed in username, then find from database on email
-    else user = await UserDB(req.body.username);
+        const errors = validationResult(req);
 
-    let connections = await userProfileDB.getUserProfile(user.userID);      //given userID, find all userConnections
+        console.log(errors.errors[0]);
+        if (!errors.isEmpty()) {
+            if (errors.errors[0].param == 'username') {            //checks if username is the first error
+                errs.push("username must be an email address");
+                if (errors.errors[1].param == 'password') errs.push("password must be at least 5 characters");     //checks  if the password is invalid, given the username is invalid
+            }
+            else if (errors.errors[0].param == 'password') errs.push("password must be at least 5 characters");    //checks if the password is invalid, given the username is valid
 
-    req.session.user = new userProfile(user, connections);
+            return res.render('login', { user: req.session.user, errors: errs });
+        }
+        let user;
 
-    res.redirect('/savedConnections');
+        if (!req.body.username) user = await UserDB("rcolli27@uncc.edu");       //use rcolli27@uncc.edu as default or the passed in username, then find from database on email
+        else user = await UserDB(req.body.username);
+
+        let connections = await userProfileDB.getUserProfile(user.userID);      //given userID, find all userConnections
+
+        req.session.user = new userProfile(user, connections);
+
+        res.redirect('/savedConnections');
 });
 
 
